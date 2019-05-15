bc0fc4acecf9bb6c87e076cca4cee17ac8202a84
Elle Elisa Ivantšikova <elleelisa@users.noreply.github.com>
2019-05-15 17:00:50 +0300

    Added another page with InkWell Widget and onTap feature

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new Column(
+          // This is the main page
+          children: <Widget>[
+            new Expanded(
+              child: new Material( // True button
+                color: Colors.greenAccent,
+                child: new InkWell(
+                  onTap: () => print("You answered true"),
+                  child: new Center(
+                    child: new Container(
+                      child: new Text("True"),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
