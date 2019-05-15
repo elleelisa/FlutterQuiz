5b305822b435491bbaaa31faca62c2421256e6f8
Elle Elisa Ivantšikova <elleelisa@users.noreply.github.com>
2019-05-15 18:30:27 +0300

    Added questions and an overlay answer

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -5,6 +5,7 @@ import '../utils/quiz.dart';
 
 import '../UI/answer_button.dart';
 import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -12,18 +13,57 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("Pizza is healthy", false),
+    new Question("Grass is green", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column(
           // This is the main page
           children: <Widget>[
-            new AnswerButton(true, () => print("You answered true")),
-            new QuestionText("Pizza is nice", 1),
-            new AnswerButton(false, () => print("You answered false")),
+            new AnswerButton(true, () => handleAnswer(true)), //true button
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)), //false button
           ],
         ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+          isCorrect,
+          () {
+            currentQuestion = quiz.nextQuestion;
+            this.setState(() {
+              overlayShouldBeVisible = false;
+              questionText = currentQuestion.question;
+              questionNumber = quiz.questionNumber;
+            });
+          }
+        ) : new Container()
       ],
     );
   }
